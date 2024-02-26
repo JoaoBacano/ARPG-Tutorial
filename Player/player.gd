b855cbe8ddef16aca90bc2c9f309960445dbfab0
JoaoBacano <91315939+JoaoBacano@users.noreply.github.com>
2024-02-26 08:52:08 +0000

    player healbar and hurtbox

--- Player/player.gd
+++ Player/player.gd
@@ -4,12 +4,19 @@ signal healthChanged
 
 @export var speed: int = 35
 @onready var animations = $AnimationPlayer
-@onready var hurtColor = $Sprite2D/ColorRect
+@onready var effects = $Effects
+@onready var hurtBox = $hurtBox
+@onready var hurtTimer = $hurtTimer
 
 @export var maxHealth: int = 3
 @onready var currentHealth: int = maxHealth
 
+@export var knockbackPower: int = 500
 
+var isHurt:bool = false
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -35,15 +42,36 @@ func handleCollision():
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
-	handleCollision()
 	updateAnimation()
+	if !isHurt:
+		for area in hurtBox.get_overlapping_areas():
+			if area.name == "hitBox":
+				hurtByEnemy(area)
 
-
-func _on_hurt_box_area_entered(area):
-	if area.name == "hitBox":
-		currentHealth -= 1
-		if currentHealth < 0:
-			currentHealth = maxHealth
-			
-		healthChanged.emit(currentHealth)
+func hurtByEnemy(area):
+	currentHealth -= 1
+	if currentHealth < 0:
+		currentHealth = maxHealth
+		
+	healthChanged.emit(currentHealth)
+	isHurt = true
 	
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+	
+func _on_hurt_box_area_entered(area): 
+	if area.has_method("collect"):
+		area.collect()
+
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
+	
+
+
+func _on_hurt_box_area_exited(area): pass
